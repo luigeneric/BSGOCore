7252235d0ec32e07ab6d91104cb63e373ec7e45f
luigeneric <42740036+luigeneric@users.noreply.github.com>
2025-08-24 04:22:20 +0200

    refactor MissionUpdater to use ScheduledExecutorService for updates and improve thread management

--- src/main/java/io/github/luigeneric/core/MissionUpdater.java
+++ src/main/java/io/github/luigeneric/core/MissionUpdater.java
@@ -2,71 +2,67 @@ package io.github.luigeneric.core;
 
 import io.github.luigeneric.core.player.counters.MissionBook;
 import io.github.luigeneric.core.protocols.player.PlayerProtocolWriteOnly;
+import io.quarkus.virtual.threads.VirtualThreads;
 import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Singleton
 public class MissionUpdater
 {
-    private static final long waitDuration = Duration.ofSeconds(3).toMillis();
+    private static final long WAIT_DURATION_BEFORE_SEND_MS = Duration.ofSeconds(1).toMillis();
+    private static final Object UPDATE_IN_PROGRESS = new Object();
 
     private final UsersContainer usersContainer;
     private final PlayerProtocolWriteOnly playerProtocolWriteOnly;
+    private final ScheduledExecutorService executorService;
 
-    private final Map<Long, Thread> usersUpdateStarted;
+    private final Map<Long, Object> usersUpdateStarted;
 
-    public MissionUpdater(UsersContainer usersContainer, PlayerProtocolWriteOnly playerProtocolWriteOnly)
+    public MissionUpdater(
+            UsersContainer usersContainer,
+            PlayerProtocolWriteOnly playerProtocolWriteOnly,
+            ScheduledExecutorService executorService
+    )
     {
         this.usersContainer = usersContainer;
         this.playerProtocolWriteOnly = playerProtocolWriteOnly;
         this.usersUpdateStarted = new ConcurrentHashMap<>();
+        this.executorService = executorService;
         log.info("MissionUpdater initialization finished");
     }
 
     public void updateRequired(final long userId)
     {
-        //log.info("updateRequired={}", userId);
-        final boolean alreadyPresent = usersUpdateStarted.containsKey(userId);
-        //if present
-        if (alreadyPresent)
+        if (usersUpdateStarted.putIfAbsent(userId, UPDATE_IN_PROGRESS) != null)
         {
-            //log.info("already present");
             return;
         }
-        //log.info("not present");
 
-        final Optional<User> optUser = usersContainer.get(userId);
-        if (optUser.isEmpty())
-        {
-            //log.info("remove user because hes empty");
-            return;
-        }
-        //log.info("initialized update");
-        final User user = optUser.get();
-        final MissionBook missionBook = user.getPlayer().getCounterFacade().missionBook();
-
-        final Thread virualThread = Thread.ofVirtual().start(() ->
-        {
-            try
-            {
-                //log.info("VThread set to sleep");
-                Thread.sleep(waitDuration);
-                //log.info("sleep finished");
-                user.send(playerProtocolWriteOnly.writeMissions(missionBook));
-                //log.info("write missions finished");
-                usersUpdateStarted.remove(userId);
-                //log.info("remove");
-            } catch (InterruptedException e)
-            {
-                log.error(e.getMessage());
-            }
-        });
-        usersUpdateStarted.put(userId, virualThread);
+        usersContainer.get(userId).ifPresentOrElse(user ->
+                {
+                    final MissionBook missionBook = user.getPlayer().getCounterFacade().missionBook();
+                    executorService.schedule(() ->
+                    {
+                        try
+                        {
+                            user.send(playerProtocolWriteOnly.writeMissions(missionBook));
+                        } catch (Exception ex)
+                        {
+                            log.error("Error in MissionUpdater", ex);
+                        } finally
+                        {
+                            usersUpdateStarted.remove(userId);
+                        }
+                    }, WAIT_DURATION_BEFORE_SEND_MS, TimeUnit.MILLISECONDS);
+                },
+                () -> usersUpdateStarted.remove(userId)
+        );
     }
 }
